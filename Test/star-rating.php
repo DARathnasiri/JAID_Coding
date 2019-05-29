6ea8bbc973f1dded304370772a3e1490b99fd20d
DARathnasiri <dilankar.17@cse.mrt.ac.lk>
2019-05-30 01:41:22 +0530

    Update star-rating.php

--- Test/star-rating.php
+++ Test/star-rating.php
@@ -36,22 +36,27 @@
 			$("#s1").click(function(){
 				$(".fa-star").css("color","black");
 				$("#s1").css("color","yellow");
+				$rate=1;
 			});
 			$("#s2").click(function(){
 				$(".fa-star").css("color","black");
 				$("#s1,#s2").css("color","yellow");
+				$rate=1;
 			});
 			$("#s3").click(function(){
 				$(".fa-star").css("color","black");
 				$("#s1,#s2,#s3").css("color","yellow");
+				$rate=1;
 			});
 			$("#s4").click(function(){
 				$(".fa-star").css("color","black");
 				$("#s1,#s2,#s3,#s4").css("color","yellow");
+				$rate=1;
 			});
 			$("#s5").click(function(){
 				$(".fa-star").css("color","black");
 				$("#s1,#s2,#s3,#s4,#s5").css("color","yellow");
+				$rate=1;
 			});
 		});
 	</script>
